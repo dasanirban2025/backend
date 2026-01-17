d9829fd222e483e1576484d004e1ddff5dc8d09c
Anirban Das <dasanirban2025@gmail.com>
2026-01-17 22:40:20 +0530

    remove app.run for gunicorn render deploy

--- app.py
+++ app.py
@@ -1,3 +1,45 @@
+# from flask import Flask, jsonify
+# from flask_cors import CORS
+# import pandas as pd
+# from model import train_model
+
+# app = Flask(__name__)
+# CORS(app)
+
+# model, scaler, features = train_model()
+
+# cols = ["engine_id","cycle"] + [f"op_{i}" for i in range(1,4)] + [f"s_{i}" for i in range(1,22)]
+# # test = pd.read_csv("test_FD001.txt", sep=" ", header=None).iloc[:, :len(cols)]
+# test = pd.read_csv(
+#     "test_FD001.txt",
+#     sep=r"\s+",
+#     header=None
+# ).iloc[:, :len(cols)]
+
+# test.columns = cols
+
+# @app.route("/engine/<int:eid>")
+# def engine_data(eid):
+#     df = test[test.engine_id == eid]
+#     X = scaler.transform(df[features])
+#     df["RUL"] = model.predict(X)
+
+#     max_rul = df["RUL"].max()
+#     df["health"] = (df["RUL"] / max_rul) * 100
+
+#     return jsonify(df[["cycle","health","RUL"]].to_dict(orient="records"))
+
+# @app.route("/engines")
+# def engines():
+#     return jsonify(sorted(test.engine_id.unique().tolist()))
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
+
+
+
 from flask import Flask, jsonify
 from flask_cors import CORS
 import pandas as pd
@@ -9,7 +51,6 @@ CORS(app)
 model, scaler, features = train_model()
 
 cols = ["engine_id","cycle"] + [f"op_{i}" for i in range(1,4)] + [f"s_{i}" for i in range(1,22)]
-# test = pd.read_csv("test_FD001.txt", sep=" ", header=None).iloc[:, :len(cols)]
 test = pd.read_csv(
     "test_FD001.txt",
     sep=r"\s+",
@@ -20,18 +61,18 @@ test.columns = cols
 
 @app.route("/engine/<int:eid>")
 def engine_data(eid):
-    df = test[test.engine_id == eid]
+    df = test[test.engine_id == eid].copy()
+    if df.empty:
+        return jsonify({"error": "Engine ID not found"}), 404
+
     X = scaler.transform(df[features])
-    df["RUL"] = model.predict(X)
+    df["RUL"] = model.predict(X).ravel()
 
     max_rul = df["RUL"].max()
-    df["health"] = (df["RUL"] / max_rul) * 100
+    df["health"] = (df["RUL"] / max_rul * 100) if max_rul > 0 else 0
 
     return jsonify(df[["cycle","health","RUL"]].to_dict(orient="records"))
 
 @app.route("/engines")
 def engines():
     return jsonify(sorted(test.engine_id.unique().tolist()))
-
-if __name__ == "__main__":
-    app.run(debug=True)
